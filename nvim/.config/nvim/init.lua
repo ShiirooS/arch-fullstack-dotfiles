ec0c388bd7f1c547a68636cb63ad4221ae616b29
Shiro <152921394+ShiirooS@users.noreply.github.com>
2026-09-20 10:29:47 -0500

    Add Neovim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.signcolumn = "yes"
+
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>")
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<cr>")
